5fd321c529d7c729e4aa48eeecc61a5f0e25eb4e
ekifatin <91857033+ekifatin@users.noreply.github.com>
2021-10-16 10:30:14 +0700

    Add files via upload

---
+++ 1_XIRPL7_5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+        $a = 1;
+        $b = 2;
+        $c = $a.$b;
+        $d = $c + 1;
+        echo $d;
+        $e = "Number";
+        $f = $e.$d;
+        echo $f;
+    ?>
+</body>
+</html>
